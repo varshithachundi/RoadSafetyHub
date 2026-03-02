baf0289d1c78c22bbb99c2532a6ade6ad00dcc1c
MY PC <MY PC@DESKTOP-U6A7841>
2026-03-02 14:22:21 +0530

    updated model

--- RoadSafetyHub/src/main/java/com/traffic/model/Payment.java
+++ RoadSafetyHub/src/main/java/com/traffic/model/Payment.java
@@ -1,5 +1,69 @@
 package com.traffic.model;
 
+import java.time.LocalDateTime;
+
 public class Payment {
+	private int paymentId;
+	private int violationId;
+	private double amount;
+	private LocalDateTime paymentDate;
+	private String paymentMethod;
+
+	public Payment() {
+	}
+
+	public Payment(int paymentId, int violationId, double amount, LocalDateTime paymentDate, String paymentMethod) {
+		this.paymentId = paymentId;
+		this.violationId = violationId;
+		this.amount = amount;
+		this.paymentDate = paymentDate;
+		this.paymentMethod = paymentMethod;
+	}
+
+	public int getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public int getViolationId() {
+		return violationId;
+	}
+
+	public void setViolationId(int violationId) {
+		this.violationId = violationId;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDateTime getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDateTime paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [paymentId=" + paymentId + ", violationId=" + violationId + ", amount=" + amount
+				+ ", paymentDate=" + paymentDate + ", paymentMethod=" + paymentMethod + "]";
+	}
 
 }

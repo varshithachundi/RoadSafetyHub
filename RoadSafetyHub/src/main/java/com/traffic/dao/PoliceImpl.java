6f1cf4db3f1e81fc79dd275fef21199e77ecaebe
MY PC <MY PC@DESKTOP-U6A7841>
2026-03-02 14:48:33 +0530

    updated police implementation

---
+++ RoadSafetyHub/src/main/java/com/traffic/dao/PoliceImpl.java
@@ -0,0 +1,95 @@
+package com.traffic.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import com.traffic.model.PoliceOfficer;
+import com.traffic.utility.DBConnection;
+
+public class PoliceImpl implements PoliceInterface{
+
+	@Override
+	public boolean addPoliceOfficer(PoliceOfficer officer) {
+		String sql = "INSERT INTO policeofficers(name, badge_number, user_id) VALUES(?,?,?)";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, officer.getName());
+            ps.setString(2, officer.getBadgeNumber());
+            ps.setInt(3, officer.getUserId());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+	}
+
+	@Override
+	public PoliceOfficer getPoliceOfficerById(int officerId) {
+		String sql = "SELECT * FROM policeofficers WHERE officer_id=?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, officerId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                PoliceOfficer p = new PoliceOfficer();
+                p.setOfficerId(rs.getInt("officer_id"));
+                p.setName(rs.getString("name"));
+                p.setBadgeNumber(rs.getString("badge_number"));
+                p.setUserId(rs.getInt("user_id"));
+                return p;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+	}
+
+	@Override
+	public List<PoliceOfficer> getAllPoliceOfficers() {
+		List<PoliceOfficer> list = new ArrayList<>();
+        String sql = "SELECT * FROM policeofficers";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                PoliceOfficer p = new PoliceOfficer();
+                p.setOfficerId(rs.getInt("officer_id"));
+                p.setName(rs.getString("name"));
+                p.setBadgeNumber(rs.getString("badge_number"));
+                p.setUserId(rs.getInt("user_id"));
+                list.add(p);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+		return list;
+	}
+
+	@Override
+	public boolean updatePoliceOfficer(PoliceOfficer officer) {
+		return false;
+	}
+
+	@Override
+	public boolean deletePoliceOfficer(int officerId) {
+		return false;
+	}
+
+}
--- RoadSafetyHub/src/main/java/com/traffic/dao/PoliceInterface.java
+++ RoadSafetyHub/src/main/java/com/traffic/dao/PoliceInterface.java
@@ -1,5 +1,17 @@
 package com.traffic.dao;
 
-public interface PoliceInterface {
+import java.util.List;
 
+import com.traffic.model.PoliceOfficer;
+
+public interface PoliceInterface {
+	boolean addPoliceOfficer(PoliceOfficer officer);
+
+	PoliceOfficer getPoliceOfficerById(int officerId);
+
+	List<PoliceOfficer> getAllPoliceOfficers();
+
+	boolean updatePoliceOfficer(PoliceOfficer officer);
+
+	boolean deletePoliceOfficer(int officerId);
 }
